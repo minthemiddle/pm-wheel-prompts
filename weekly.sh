7cbec9626438af6d3d477d71ddd6fd3eb6c369c3
Martin Betz <martinmichaelbetz@gmail.com>
2024-10-14 15:47:28 +0200

    Improve: Enhance weekly challenge output format
    - Consolidate echo statements into a single output variable for clarity.
    - Add script directory handling for better path management.
    - Append challenge options retrieval directly to the output variable.
    - Copy final formatted output to clipboard for user convenience.

--- weekly.sh
+++ weekly.sh
@@ -2,21 +2,22 @@
 
 week=$(date +%U)
 
-echo "Weekly Product Challenge - CW${week}"
-echo
+output="Weekly Product Challenge - CW${week}\n\n"
+output+="Here are your options for week ${week}'s challenge.\n"
+output+="Pick one area that you want to improve in and work on the challenge for at least 5min.\n"
+output+="Share your learnings and progress in the One-on-One with your manager and/or publicly in this thread.\n\n"
 
-cat <<EOT
-Here are your options for week ${week}'s challenge.
-Pick one area that you want to improve in and work on the challenge for at least 5min.
-Share your learnings and progress in the One-on-One with your manager and/or publicly in this thread.
+# Get the directory of the script
+script_dir=$(dirname "$0")
 
-EOT
-
-sqlite3 -header database.sqlite < script.sql | awk '
+# Append the output of the sqlite3 and awk commands to the variable
+output+=$(sqlite3 -header $script_dir/database.sqlite < $script_dir/script.sql | awk '
   BEGIN { FS = "|" }
   NR == 1 { next }
   {
-    printf("%s\n%s\n", $1, $2)
-    print ""
+    printf("%s\n%s\n\n", $1, $2)
   }
-'
+')
+
+# Copy the output to the clipboard
+echo -e "$output" | pbcopy
